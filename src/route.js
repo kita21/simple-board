0b34a8b531402b76c76ef653443c65f950cf8a3b
Kita <syusyufortis337@icloud.com>
2020-02-14 08:56:27 +0900

    accountぺーじ追加

--- src/route.js
+++ src/route.js
@@ -1,9 +1,11 @@
 const IndexController = require('./controllers/index'),
     BoardController = require('./controllers/board'),
-    UserController = require('./controllers/user')
+    UserController = require('./controllers/user'),
+    AccountController = require('./controllers/account');
 
 exports.routing = (app) => {
 
     app.use('', IndexController);
     app.use('/user', UserController);
+    app.use('/account', AccountController);
 }
